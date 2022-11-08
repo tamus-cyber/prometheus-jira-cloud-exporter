c6ea1dc624fc76d3b10b25493b81054c20272d54
tyleraharrison <tyleraharrison@gmail.com>
2022-11-08 17:36:36 -0600

    Added breached SLAs
    Signed-off-by: tyleraharrison <tyleraharrison@gmail.com>

--- issue_collector.py
+++ issue_collector.py
@@ -33,7 +33,11 @@ class IssueCollector:
         try:
             with open("custom_field_map.json", "r") as f:
                 self.custom_fields = json.load(f)
-                self.custom_fields_str = ", ".join(self.custom_fields.keys())
+                # If any keys have a ., only keep the part before the .
+                parsed_custom_fields = {
+                    k.split(".")[0]: v for k, v in self.custom_fields.items()
+                }
+                self.custom_fields_str = ",".join(parsed_custom_fields.keys())
         except FileNotFoundError:
             self.custom_fields = {}
             self.custom_fields_str = ""
@@ -45,6 +49,9 @@ class IssueCollector:
             # Loop over the JQL results
             while bool(result):
                 for issue in result:
+                    # Write to JSON
+                    with open("test.json", "w") as f:
+                        json.dump(issue.raw, f, indent=4)
                     # Assign Jira attributes to variables
                     project = str(issue.fields.project)
                     project_name = str(issue.fields.project.name)
@@ -64,7 +71,11 @@ class IssueCollector:
                     # Get custom fields
                     custom_fields = {}
                     for key, value in self.custom_fields.items():
-                        custom_fields[value] = str(issue.fields.__dict__[key])
+                        # If the key has a . in it, it's a nested field, use recursion to get the value
+                        if "." in key:
+                            custom_fields[value] = self.get_nested_field(issue.raw['fields'], key)
+                        else:
+                            custom_fields[value] = str(issue.fields.__dict__[key])
 
                     # Construct the list of labels from attributes
                     prom_label = [
@@ -147,3 +158,68 @@ class IssueCollector:
         for labels, value in self.prom_output.items():
             issues_gauge.add_metric(labels, value)
         yield issues_gauge
+
+    
+    @classmethod
+    def walktree(self, tree, at=lambda node: not isinstance(node, dict), prefix=(), 
+                flattennode=lambda node:isinstance(node, (list, tuple, set))):
+        """
+        Traverse a tree, and return a iterator of the paths from the root nodes to the leaf nodes.
+        tree: like '{'a':{'b':1,'c':2}}'
+        at: a bool function or a int indicates levels num to go down. walktree(tree, at=1) equivalent to tree.items()
+        flattennode: a bool function to decide whether to iterate at node value
+        """
+        if isinstance(at, int):
+            isleaf_ = at == 0
+            isleaf = lambda v: isleaf_
+            at = at - 1
+        else:
+            isleaf = at
+        if isleaf(tree):
+            if not flattennode(tree):
+                yield (*prefix, tree)
+            else:
+                for v in tree:
+                    yield from self.walktree(v, at, prefix, flattennode=flattennode)
+        else:
+            for k,v in tree.items():
+                yield from self.walktree(v, at, (*prefix, k), flattennode=flattennode)   
+
+
+    @classmethod
+    def get_nested_field(self, issue, key):
+        """Take a nested field and return the value
+        
+        Example: key = "customfield_10000.value[0].value"
+        Example: issue = {"customfield_10000": {"value": [{"value": "foo"}]}}
+
+        Args:
+            issue: The issue to get the field from
+            key: The key to get the value from
+
+        Returns:
+            value: The value of the field
+        """
+        # Split the key into a list
+        key_list = key.split(".")
+        # Get the first key
+        first_key = key_list.pop(0)
+        # If the key is a *, then we traverse every key in the dict to find the value after the *
+        if first_key == "*":
+            # Get the value after the *
+            value_key = key_list.pop(0)
+            # Walk the tree and unpack into lists of all possible leaf nodes
+            unpacked = list(self.walktree(issue))
+            # Check each leaf node to see if it matches the value key
+            for item in unpacked:
+                # If the value key matches, return the value
+                # Example: ('completedCycles', 'breached', True) when looking for "customfield_10600.*.breached"
+                if item[-2] == value_key:
+                    return str(item[-1])
+            
+        value = issue[first_key]
+        # If there are more keys, recurse
+        if key_list:
+            return self.get_nested_field(value, ".".join(key_list))
+        else:
+            return str(value)
